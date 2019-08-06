31c12fc9b373dab2bc61f43434edef27d1281317
victor hugo <victor03016@gmail.com>
2019-08-06 17:45:47 +0200

    Mostrando los Productos

--- src/components/ProductoLista.js
+++ src/components/ProductoLista.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function ProductoLista({producto}) {
-return(
-    <div>{producto.nombrePlatillo}</div>
-);
+function ProductoLista({ producto }) {
 
+    const eliminarProducto = id => {
+        console.log('Eliminar ' + id);
+
+        // TODO Eliminar los registros
+    } 
+
+    return (
+        <li
+            className="list-group-item d-flex justify-content-between align-items-center"
+            data-categoria={producto.categoria}
+        >
+            <p>
+                {producto.nombrePlatillo}{" "}
+                <span className="font-weight-bold">
+                    $ {producto.precioPlatillo}
+                </span>
+            </p>
+            <div>
+                <Link
+                    to={`/productos/editar/${producto.id}`}
+                    className="btn btn-success mr-2"
+                >
+                    Editar
+                </Link>
+                <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => eliminarProducto(producto.id)}
+                >
+                    Eliminar &times;
+                </button>
+            </div>
+        </li>
+    );
 }
 
 export default ProductoLista;
